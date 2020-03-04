23e2a011935fe40b3dbd3c455365eb02e00be513
j3diniz <j3di@uol.com.br>
2020-03-04 15:42:21 -0300

    Getting parameter values

---
+++ IntroToRevitAPI20200109/EditParameters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace IntroToRevitAPI20200109 {
+
+    [Transaction(TransactionMode.Manual)]
+    class EditParameters : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+			try {
+                UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+                Document doc = uidoc.Document;
+
+                FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+                ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_Windows);
+
+                IList<Element> windows = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
+
+                foreach (Element ele in windows) {
+                    Parameter para = ele.LookupParameter("Head Height");
+                    string storage = para.StorageType.ToString();
+                    double value = para.AsDouble();
+                    TaskDialog.Show("Parameters", "Parameter is a " + storage + " with value: " + value.ToString());
+                }
+
+                TaskDialog.Show("Windows", windows.Count + " Windows Found!");
+
+                return Result.Succeeded;
+
+            } catch (Exception ex) {
+                TaskDialog.Show("Revit API Sample", "App fail: " + ex.Message);
+                return Result.Failed;
+            }
+        }
+    }
+}
